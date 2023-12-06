7678a73d6440da38d28669cdecc053e694cd5f41
Ramkalame <ramkalame14082001@gmail.com>
2023-12-06 12:00:30 +0530

    exception handling with overriding method

---
+++ JavaCorePractice/src/com/java/practice/day3/CheckedException.java
@@ -0,0 +1,15 @@
+package com.java.practice.day3;
+
+public class CheckedException {
+	
+	
+	void printHello() {
+		System.out.println("hellow ram");
+	}
+	
+	public void greet() {
+		
+		System.out.println("hellow sir , GoodMorning");
+	}
+
+}
---
+++ JavaCorePractice/src/com/java/practice/day3/ParentClasswithException.java
@@ -0,0 +1,10 @@
+package com.java.practice.day3;
+
+public class ParentClasswithException {
+	
+	void msg()throws ArithmeticException{
+		
+		System.out.println("Parent Method");
+	}
+
+}
